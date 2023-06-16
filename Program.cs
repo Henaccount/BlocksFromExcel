4d1c30997a84160f5805cd7c952d2df36e09dc0f
Henaccount <36333524+Henaccount@users.noreply.github.com>
2023-06-16 14:43:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,110 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace BlocksFromExcel
+{
+    public class BlocksFromExcel
+    {
+        [CommandMethod("BlocksFromExcel")]
+        public static void ImportFromExcel()
+        {
+            // Get the active AutoCAD document and database
+            Document doc = Application.DocumentManager.MdiActiveDocument;
+            Database db = doc.Database;
+            Editor editor = doc.Editor;
+
+            // Prompt user to select an Excel file
+            PromptOpenFileOptions options = new PromptOpenFileOptions("Select Excel file");
+            options.Filter = "Excel Files (*.xlsx)|*.xlsx";
+            PromptFileNameResult result = editor.GetFileNameForOpen(options);
+            if (result.Status != PromptStatus.OK)
+            {
+                editor.WriteMessage("\nInvalid file selection.");
+                return;
+            }
+
+            // Read Excel data and insert blocks into the drawing
+            string excelFilePath = result.StringResult;
+            Excel.Application excelApp = new Excel.Application();
+            Excel.Workbook workbook = excelApp.Workbooks.Open(excelFilePath);
+            Excel.Worksheet worksheet = workbook.ActiveSheet;
+            Excel.Range usedRange = worksheet.UsedRange;
+
+            try
+            {
+                int rowCount = usedRange.Rows.Count;
+                int columnCount = usedRange.Columns.Count;
+
+                // Iterate over each row in the used range
+                for (int row = 2; row <= rowCount; row++) // Assuming header row is skipped
+                {
+                    // Read blockname and tag from the Excel columns
+                    string blockName = usedRange.Cells[row, 1].Value.ToString();
+                    string tag = usedRange.Cells[row, 2].Value.ToString();
+                    ObjectId curblock = new ObjectId();
+                    // Insert the block into the drawing
+                    using (Transaction transaction = db.TransactionManager.StartTransaction())
+                    {
+                        BlockTable blockTable = transaction.GetObject(db.BlockTableId, OpenMode.ForRead) as BlockTable;
+                        BlockTableRecord modelSpace = transaction.GetObject(blockTable[BlockTableRecord.ModelSpace], OpenMode.ForWrite) as BlockTableRecord;
+                        //editor.WriteMessage("\nblockName: " + blockName);
+                        //editor.WriteMessage("\ntag: " + tag);
+
+
+                        using (BlockReference blockRef = new BlockReference(new Point3d(0, row * -5000, 0), blockTable[blockName]))
+                        {
+                            curblock = modelSpace.AppendEntity(blockRef);
+                            transaction.AddNewlyCreatedDBObject(blockRef, true);
+
+                        }
+
+                        transaction.Commit();
+
+
+                    }
+
+                    editor.Command("attsync", "S", curblock, "Y");
+
+                    using (Transaction transaction = db.TransactionManager.StartTransaction())
+                    {
+                        BlockReference blockinstRef = transaction.GetObject(curblock, OpenMode.ForWrite) as BlockReference;
+                        //AttributeReference attributeRef = transaction.GetObject(nestedResult.ObjectId, OpenMode.ForWrite) as AttributeReference;
+
+                        foreach (ObjectId attributeId in blockinstRef.AttributeCollection)
+                        {
+                            AttributeReference attribute = transaction.GetObject(attributeId, OpenMode.ForWrite) as AttributeReference;
+                            editor.WriteMessage("\nattribute.Tag: " + attribute.Tag);
+                            if (attribute.Tag == "FDSA")
+                            {
+                                editor.WriteMessage("\nfound attribute and want to update with: " + tag);
+                                attribute.TextString = tag;
+                            }
+                        }
+
+
+
+                        transaction.Commit();
+                    }
+                }
+
+                editor.WriteMessage($"\n{rowCount - 1} blocks inserted into the drawing.");
+            }
+            catch (System.Exception ex)
+            {
+                editor.WriteMessage($"\nAn error occurred: {ex.Message + "\n" + ex.StackTrace}");
+            }
+            finally
+            {
+                workbook.Close();
+                excelApp.Quit();
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(worksheet);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(workbook);
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(excelApp);
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,41 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BlocksFromExcel")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BlocksFromExcel")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("A9C68C73-8DC5-4848-9193-07F4CE00DB88")]
+
+
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+//v1:changed to fit certain iso style (non-default)
+//v2:more changes, bug fixes
+//v3: elbow elevation-, DN labels- translation, precision argument for metric to imperial
+[assembly: AssemblyVersion("24.24.0.0")]
+[assembly: AssemblyFileVersion("24.24.0.0")]
